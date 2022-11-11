302c07bcad69e467e9f499e4c86a20c4a1654862
Vit <vit_waladzko@mail.ru>
2022-11-11 14:48:16 +0300

    test component style

--- src/features/auth/Test/Test.tsx
+++ src/features/auth/Test/Test.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import {InputText} from "../../../main/ui/common/components/InputText/InputText";
 import {Button} from "../../../main/ui/common/components/Button/Button";
 import {Checkbox} from "../../../main/ui/common/components/CheckBox/Checkbox";
+import style from './Test.module.css'
 
 export const Test = () => {
     return (
-        <div>
-            <div>Test</div>
-            <InputText />
-            <InputText />
-            <Button>SUBMIT</Button>
-            <Checkbox/>
+        <div className={style.test}>
+            <h1>Test</h1>
+            <div className={style.inputGroup}>
+                <InputText/>
+                <InputText/>
+            </div>
+            <div className={style.btnGroup}>
+                <Button>SUBMIT</Button>
+                <Checkbox/>
+            </div>
         </div>
     )
 }
