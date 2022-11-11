9aeeae8525008ce6b12befe2abd0dbb7d9b3c425
Vit <vit_waladzko@mail.ru>
2022-11-11 14:04:56 +0300

    test component

--- src/features/auth/Test/Test.tsx
+++ src/features/auth/Test/Test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {InputText} from "../../../main/ui/common/components/InputText/InputText";
+import {Button} from "../../../main/ui/common/components/Button/Button";
+import {Checkbox} from "../../../main/ui/common/components/CheckBox/Checkbox";
 
 export const Test = () => {
     return (
         <div>
-            Test
+            <div>Test</div>
+            <InputText />
+            <InputText />
+            <Button>SUBMIT</Button>
+            <Checkbox/>
         </div>
     )
 }
---
+++ src/main/ui/common/components/InputText/InputText.tsx
@@ -0,0 +1,56 @@
+import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes, KeyboardEvent} from 'react';
+import s from './InputText.module.css';
+
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+
+type SuperInputTextPropsType = DefaultInputPropsType & {
+    onChangeText?: (value: string) => void
+    onEnter?: () => void
+    error?: string
+    spanClassName?: string
+}
+
+export const InputText: React.FC<SuperInputTextPropsType> = (
+    {
+        type,
+        onChange, onChangeText,
+        onKeyPress, onEnter,
+        error,
+        className, spanClassName,
+
+        ...restProps
+    }
+) => {
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange
+        && onChange(e)
+
+        onChangeText && onChangeText(e.currentTarget.value)
+    }
+    const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
+        onKeyPress && onKeyPress(e);
+
+        onEnter
+        && e.key === 'Enter'
+        && onEnter()
+    }
+
+    const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`;
+    const finalInputClassName = `${s.errorInput} ${error ? s.errorInput : s.superInput} ${className}`;
+
+
+
+    return (
+        <>
+            <input
+                type={type}
+                onChange={onChangeCallback}
+                onKeyPress={onKeyPressCallback}
+                className={finalInputClassName}
+
+                {...restProps}
+            />
+            {error && <span className={finalSpanClassName}>{error}</span>}
+        </>
+    );
+}
