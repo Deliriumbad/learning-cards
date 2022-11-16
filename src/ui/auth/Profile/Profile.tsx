fae1bb365024bf2dfa231ca6b80c1f52dffc907a
Steel <Steelmen87@bk.ru>
2022-11-16 16:20:18 +0300

    log out +- add

--- src/ui/auth/Profile/Profile.tsx
+++ src/ui/auth/Profile/Profile.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import { logOutAC } from 'bll/reducers/profile-reducer';
+import { useDispatch } from 'react-redux';
+
+import logout from '../../../common/img/logout.png';
 import style from '../../../style/style.module.css';
-import Button from '../../components/Button/Button';
 
 import s from './style.module.css';
+import { useNavigate } from 'react-router-dom';
+import { useAppSelector } from '../../../bll/store/hooks';
 
 const Profile = () => {
+    const isLogOut = useAppSelector(state => state.profile.isLogOut);
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (isLogOut) {
+            isLogOut && navigate('/');
+        }
+    }, [isLogOut]);
+
+
+    const dispatch = useDispatch();
+
+    const onClickHandler = () => {
+        dispatch(logOutAC());
+    };
     return (
         <div className={style.container}>
             <div className={s.card_style}>
@@ -13,7 +33,10 @@ const Profile = () => {
                 <div className={s.photo}>Photo</div>
                 <div className={s.name}>Ivan</div>
                 <div className={s.email}>j&johnson@gmail.com</div>
-                <Button className={s.button_style}>log out</Button>
+                <div className={style.button_style} onClick={onClickHandler}>
+                    <img src={logout} className={s.log_out_png} alt='img' />
+                    <div className={s.title_button}>log out</div>
+                </div>
             </div>
         </div>
     );
