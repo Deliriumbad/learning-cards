9c1462a5f07a77c4e490d659a51f5dc110eeef91
Vit <vit_waladzko@mail.ru>
2022-11-16 05:37:24 +0300

    feat: add styles

--- src/ui/auth/Login/Login.tsx
+++ src/ui/auth/Login/Login.tsx
@@ -53,30 +53,48 @@ const Login = () => {
     }   */
     return (
         <form onSubmit={formik.handleSubmit} className={s.form}>
-            <label htmlFor="email">
+            <div className={s.title}>Sign in</div>
+            <label htmlFor="email" className={s.label}>
                 Email
-                <InputText type="email" id="email" {...formik.getFieldProps('email')} />
+                <InputText
+                    type="email"
+                    id="email"
+                    className={s.mailInput}
+                    {...formik.getFieldProps('email')}
+                />
             </label>
             {formik.errors.email && formik.touched.email && (
-                <div style={{ color: 'red' }}>{formik.errors.email}</div>
+                <span className={s.error}>{formik.errors.email}</span>
             )}
-            <label htmlFor="password">
+            <label htmlFor="password" className={s.label}>
                 Password
-                <InputText type="password" id="password" {...formik.getFieldProps('password')} />
+                <InputText
+                    type="password"
+                    id="password"
+                    className={s.passInput}
+                    {...formik.getFieldProps('password')}
+                />
             </label>
             {formik.errors.password && formik.touched.password && (
-                <div style={{ color: 'red' }}>{formik.errors.password}</div>
+                <span className={s.error}>{formik.errors.password}</span>
             )}
-            <label>
+            <label className={s.checkbox}>
                 <Checkbox
                     {...formik.getFieldProps('rememberMe')}
                     checked={formik.values.rememberMe}
                 />{' '}
                 Remember me
-                <NavLink to={PATH.passwordRecovery}>Forgot password?</NavLink>
             </label>
-            <Button type="submit">Login</Button>
-            <NavLink to={PATH.registration}>Sign Up</NavLink>
+            <NavLink to={PATH.passwordRecovery} className={s.forgotPass}>
+                Forgot password?
+            </NavLink>
+            <Button type="submit" className={s.button}>
+                Sign In
+            </Button>
+            <div className={s.message}>Already have an account?</div>
+            <NavLink to={PATH.registration} className={s.signUp}>
+                Sign Up
+            </NavLink>
         </form>
     );
 };
