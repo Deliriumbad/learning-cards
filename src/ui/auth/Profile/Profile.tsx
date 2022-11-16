cfb20a0c4676a1bdf8ba1b79d508ac04310284d4
Steel <Steelmen87@bk.ru>
2022-11-16 13:56:17 +0300

    add card

--- src/ui/auth/Profile/Profile.tsx
+++ src/ui/auth/Profile/Profile.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import style from '../../../style/style.module.css';
+import Button from '../../components/Button/Button';
+
+import s from './style.module.css';
+
 const Profile = () => {
-    return <div>Profile</div>;
+    return (
+        <div className={style.container}>
+            <div className={s.card_style}>
+                <div className={s.title}>Personal Information</div>
+                <div className={s.photo}>Photo</div>
+                <div className={s.name}>Ivan</div>
+                <div className={s.email}>j&johnson@gmail.com</div>
+                <Button className={s.button_style}>log out</Button>
+            </div>
+        </div>
+    );
 };
 
 export default Profile;
