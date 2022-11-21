692a2af963d0e8333842c41d07e31553364034d1
Oleg655 <savinoleg95@gmail.com>
2022-11-21 21:46:44 +0300

    feat: create hook for pages

--- src/ui/packs/Packs.tsx
+++ src/ui/packs/Packs.tsx
@@ -62,8 +62,8 @@ const Packs = () => {
                         ))}
                     </tbody>
                 </table>
+                <Pagination />
             </div>
-            <Pagination />
         </>
     );
 };
